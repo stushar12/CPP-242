793638606cf7fa2c0d653ae08d737411a1060f02
Tushar Saxena <stushar345@gmail.com>
2021-06-06 02:17:11 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,27 +1,45 @@
-vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> ans;
-        if(n<3)
-            return ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i++){
-            int j=i+1,k=n-1;
-            while(j<k){
-                int sum=nums[k]+nums[j]+nums[i];
-                if(sum==0){
-                    ans.push_back({nums[i],nums[j],nums[k]});
-                    while(j<k&&nums[j]==nums[j+1])
-                        j++;
-                    while(j<k&&nums[k]==nums[k-1])
-                        k--;
-                    j++; k--;
-                }
-                else if(sum>0)
-                    k--;
-                else j++;
-            }
-            while(i<n-2&&nums[i]==nums[i+1])
-                i++;
-        }
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    int target = 0;
+    int n = nums.size();
+    vector<vector<int>> ans;
+    if (n < 3)
         return ans;
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k)
+        {
+            vector<int> v;
+            int sum = nums[i] + nums[j] + nums[k];
+
+            if (sum == target)
+            {
+                v.push_back(nums[i]);
+                v.push_back(nums[j]);
+                v.push_back(nums[k]);
+                ans.push_back(v);
+                while (j < k and nums[j] == nums[j + 1])
+                    j++;
+                while (j < k and nums[k] == nums[k - 1])
+                    k--;
+
+                j++;
+                k--;
+            }
+            if (sum < target)
+                j++;
+            else
+                k--;
+        }
+        while (i < n - 2 and nums[i] == nums[i + 1])
+            i++;
     }
+    return ans;
+}
